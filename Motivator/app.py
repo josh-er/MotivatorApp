fcb0224aaa9696206f8d3fc8fd8434da170b2b95
josh-er <jeherz88@gmail.com>
2025-12-25 18:51:46 -0500

    add additional import to app.py

--- Motivator/app.py
+++ Motivator/app.py
@@ -10,6 +10,7 @@ from Motivator.db import SessionLocal, engine
 from Motivator.user_service import create_user
 from Motivator.models import User, Quote
 from Motivator.admin.routes import admin_bp
+from Motivator.send_quotes import send_quote_to_user
 
 ADMIN_KEY = os.getenv("ADMIN_KEY")
 ENV = os.getenv("ENV", "development")
