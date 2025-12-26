d71a5409573a4c723998afbd53ebb5c45be67df7
josh-er <jeherz88@gmail.com>
2025-12-26 16:06:06 -0500

    update logging to have since_date value

--- Motivator/admin/routes.py
+++ Motivator/admin/routes.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template
 from .auth import require_admin_key
 from .services import get_all_users, get_message_logs
 from flask import request
+from datetime import date
 
 admin_bp = Blueprint("admin", __name__, url_prefix="/admin")
 
@@ -16,13 +17,20 @@ def users():
 def logs():
     from Motivator.db import engine
     print("ADMIN DB URL:", engine.url)
+
     status = request.args.get("status")
     phone = request.args.get("phone")
     limit = int(request.args.get("limit", 100))
+    
+    # Default to today
+    since_date = request.args.get("since_date")
+    if not since_date:
+        since_date = date.today()
 
     logs = get_message_logs(
         status=status,
         phone=phone,
+        since_date=since_date,
         limit=limit
     )
     return render_template("admin/logs.html", logs=logs)
