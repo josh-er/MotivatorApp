44894ed4f59c57c87cbf19c200873a089c04ecdb
Josh Herz <jeherz88@gmail.com>
2026-07-05 10:01:16 -0700

    Configure base URL per build environment: localhost for debug, Render URL for release

--- MotivatorUI/MotivatorUI/Networking/APIEndpoints.swift
+++ MotivatorUI/MotivatorUI/Networking/APIEndpoints.swift
@@ -1,7 +1,11 @@
 import Foundation
 
 struct APIEndpoints {
+#if DEBUG
     static let baseURL = "http://127.0.0.1:5000"
+#else
+    static let baseURL = "https://motivatorapp.onrender.com"
+#endif
 
     static let submit = "\(baseURL)/submit"
     static let requestSettingsLink = "\(baseURL)/request-settings-link"
